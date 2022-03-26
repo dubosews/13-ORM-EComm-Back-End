304131986a060b5e29d8877f5034c4b2dfaee3ed
Wesley DuBose <wsd10205@gmail.com>
2022-03-26 03:35:57 -0400

    commit to include product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,34 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productsData = await Product.findAll({
+      include: [{ model: Category },{ model: Tag }]
+    });
+    res.status(200).json(productsData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const singleProductData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category },{ model: Tag }]
+    });
+
+    if (!singleProductData) {
+      res.status(404).json({ message: 'No Product was found with that id!' });
+      return;
+    }
+
+    res.status(200).json(singleProductData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -91,6 +113,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  try {
+    const libraryCardData = await LibraryCard.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!libraryCardData) {
+      res.status(404).json({ message: 'No library card found with that id!' });
+      return;
+    }
+
+    res.status(200).json(libraryCardData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
